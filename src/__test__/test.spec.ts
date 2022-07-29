5df1e93d92b463670e3f706f83b519d3ba2aa423
YI <57102971+london-newyork@users.noreply.github.com>
2022-07-29 23:52:47 +0900

    first commit

---
+++ src/__test__/test.spec.ts
@@ -0,0 +1,7 @@
+import { test } from '../app';
+
+describe('test', () => {
+  it('has to be Nice!', () => {
+    expect(test(2, 1));
+  });
+});
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+export const test = (a: number, b: number) => {
+  if (a + b === 3) {
+    return console.log('Nice!');
+  }
+  return;
+};
